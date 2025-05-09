c0385570f1f650cff669f21327228655b78d0f8a
GaromaMakure <garomamak11@gmail.com>
2025-05-08 23:01:48 -0700

    updating the backend side

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import { User } from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { generateTokenAndSetCookie } from "../utils/generateToken.js";
 export async function signup(req, res) {
   try {
     const { email, password, username } = req.body;
@@ -41,10 +42,12 @@ export async function signup(req, res) {
 
     const newUser = new User({
       email,
-      password:hashedPassword,
+      password: hashedPassword,
       username,
       image,
     });
+
+    generateTokenAndSetCookie(newUser._id, res);
     await newUser.save();
     res.status(201).json({
       success: true,
@@ -60,8 +63,51 @@ export async function signup(req, res) {
 }
 
 export async function login(req, res) {
-  res.send("login route");
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+    }
+
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+
+    const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      success: true,
+      user: {
+        ...user._doc,
+        password: "",
+      },
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
 }
+
 export async function logout(req, res) {
-  res.send("logout route");
+  try {
+    res.clearCookie("jwt-netflix");
+    res.status(200).json({ success: true, message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
 }
